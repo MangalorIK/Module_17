82e7913b4c43fd69637519b01266eaf9e3bf848e
MangalorIK <mangalor89@gmail.com>
2025-01-09 14:17:37 +0300

    CRUD add

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import task, user
+from app.routers import task, user
 
 app = FastAPI()
 
@@ -11,3 +11,5 @@ async def welcome():
 
 app.include_router(task.router)
 app.include_router(user.router)
+
+# uvicorn main:app --reload
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+# Валидация
+from typing import Annotated
+from app.models import User
+from app.schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+# Функция создания slug-строки
+from slugify import slugify
+
+from app.models import *
+from app.schemas import CreateUser
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 
 @router.get("/")
-async def all_users():
-    pass
+async def all_users(db: Annotated[Session, Depends(get_db)]):
+    users = db.scalars(select(User)).all()
+    return users
 
 
 @router.get("user_id")
@@ -14,15 +27,53 @@ async def user_by_id():
 
 
 @router.post("/create")
-async def create_user():
-    pass
+async def create_user(db: Annotated[Session,Depends(get_db)], create_user: CreateUser):
+    db.execute(insert(User).values(username=create_user.username,
+                                   firstname=create_user.firstname,
+                                   lastname=create_user.lastname,
+                                   age=create_user.age,
+                                   slug=slugify(create_user.username)))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
 
 
 @router.put("/update")
-async def update_user():
-    pass
+async def update_user(db: Annotated[Session, Depends(get_db)],user_id: int, update_user: CreateUser):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There is no user found"
+        )
 
+    db.execute(update(User).where(User.id == user_id).values(
+        username=update_user.username,
+        firstname=update_user.firstname,
+        lastname=update_user.lastname,
+        age=update_user.age,
+        slug=slugify(update_user.username)
+    ))
+
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'Category update is successful'
+    }
 
 @router.delete("/delete")
-async def delete_user():
-    pass
+async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There is no user found"
+        )
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': "User delete is successful"
+    }
